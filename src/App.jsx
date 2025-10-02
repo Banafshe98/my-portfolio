8ebd7654e796880cbcb8bf9c4e1c7f2fa32f03b1
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2025-10-02 10:04:46 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-neutral-300 rounded-2xl px-2 py-3 cursor-pointer">hi</div>
+    </>
+  ); 
+} 
+export default App;
