2c79cdeca073495fbc1e319e441d33dcfbdf5ed6
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2025-10-04 18:40:47 +0330

    fixed p width

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Layout(props) {
   return (
-    <div className='p-5 bg-black '>{props.children}</div>
+    <div className='p-5 bg-black w-full min-h-screen'>
+      <div className='max-w-screen-xl'>{props.children}</div>
+      </div>
   )
 }
 
