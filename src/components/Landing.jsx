580044b75f1c6b28cd54cfe7923ba78be929799d
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2025-10-07 11:23:43 +0330

    Landing page fixed for tablet

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,7 +7,7 @@ function Landing() {
     <>
       <section className="bg-black text-white p-3 py-12 h-full w-10/12 mx-auto my-16 rounded-md overflow-x-hidden">
         <div className="w-fit mx-auto">
-          <div className="text-[clamp(2rem,10vw,6rem)] md:text-[7vh]  leading-tight font-extrabold text-center text-wrap md:text-nowrap px-2 sm:px-4 md:px-6">
+          <div className="text-[clamp(2rem,10vw,6rem)] md:text-[5vh]  leading-tight font-extrabold text-center text-wrap md:text-nowrap px-2 sm:px-4 md:px-6">
             <p >FRONT-END DEVELOPER</p>
           </div>
           <div className="flex justify-center gap-3 max-w-[1200px]">
@@ -22,7 +22,7 @@ function Landing() {
         <div className="sm:pr-8 py-6">
           <img
             src={Person}
-            className="sm:rounded-tr-full sm:py-10 sm:rounded-br-full mx-auto rounded-full"
+            className="sm:rounded-tr-full sm:py-10 sm:rounded-br-full md:px-20  mx-auto rounded-full"
             alt="Banafshe Yazdani's Avatar"
           />
         </div>
