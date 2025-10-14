5b010a2937d66de34ac901a9ee47ea199318804e
Banafshe98 <152071599+Banafshe98@users.noreply.github.com>
2025-10-14 21:37:33 +0330

    fixed contact me responsive

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import emojiRegex from "emoji-regex";
-
+ 
 function ContactMe() {
   return (
-    <>
-      <div className="px-10 pt-10">
-        <p className="font-medium text-4xl">Love to hear from you,</p>
-        <p className="font-light text-3xl">Get in touch👋</p>
+    <div className="md:pt-40 md:pl-20 lg:p-44">
+      <div className="px-10 pt-10 ">
+        <p className="font-medium text-4xl md:text-5xl lg:text-6xl xl:text-7xl">Love to hear from you,</p>
+        <p className="font-light text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Get in touch👋</p>
       </div>
-      <div className="md:flex md:-mx-4 mt-4 px-6 py-6 md:mt-10 bg-amber-300">
+      <div className="md:flex md:-mx-4 mt-4 px-6 py-6 md:mt-10">
         <div className="md:w-2/3 md:px-4">
           <div className="contact-form">
             <div className="sm:flex sm:flex-wrap -mx-3">
@@ -52,7 +51,7 @@ function ContactMe() {
               </div>
             </div>
             <div className="text-right mt-4 md:mt-12">
-              <button className="border-2 border-indigo-600 rounded px-6 py-2 text-indigo-600 hover:bg-indigo-600 hover:text-white transition-colors duration-300">
+              <button className="bg-black text-white rounded px-6 py-2 md:px-16 lg:px-20 hover:bg-in hover:bg-emerald-700 cursor-pointer transition-colors duration-300">
                 Send a Message
                 <i className="fas fa-chevron-right ml-2 text-sm"></i>
               </button>
@@ -60,7 +59,7 @@ function ContactMe() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
